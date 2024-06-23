d9d1f524a43037fa18fd9d26f6d2969f234781fd
सिंकु कुमार <73785795+SinkuKumar@users.noreply.github.com>
2024-06-23 11:50:48 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import { useEffect, useState } from "react";
+
+export default function advice() {
+  const [count, setCount] = useState(0);
+  const [advice, setAdvice] = useState("Click button to get a new advice!");
+
+  async function getAdvice() {
+    const adviceSlipData = await fetch("https://api.adviceslip.com/advice");
+    const adviceSlipJson = await adviceSlipData.json();
+    setAdvice(adviceSlipJson.slip.advice);
+    setCount(() => count + 1);
+  }
+
+  useEffect(() => {
+    getAdvice();
+  }, []);
+
+  return (
+    <div>
+      <h1>{advice}</h1>
+      <button onClick={getAdvice}>Get Advice</button>
+      <p>You've got {count} advice so far.</p>
+    </div>
+  );
+}
